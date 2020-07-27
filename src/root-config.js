c4baab4a4aa64c89923228588f363d2c702fa37f
kapiladiyecha <kapiladiyecha@gmail.com>
2020-07-28 02:40:46 +0800

    POC - systemjs importmap with cdn

--- src/root-config.js
+++ src/root-config.js
@@ -1,15 +1,16 @@
+import { registerApplication, start } from "single-spa";
 import {
-  constructRoutes,
   constructApplications,
   constructLayoutEngine,
+  constructRoutes,
 } from "single-spa-layout";
-import { registerApplication, start } from "single-spa";
 
 const routes = constructRoutes(document.querySelector("#single-spa-layout"));
 const applications = constructApplications({
   routes,
   loadApp: ({ name }) => System.import(name),
 });
+
 // Delay starting the layout engine until the styleguide CSS is loaded
 const layoutEngine = constructLayoutEngine({
   routes,
